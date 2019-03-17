576b5277da0f38a98cd8feaec8fbd4df3583d644
myjoybar <myjoybar@gmail.com>
2019-03-17 21:24:13 +0800

    add charpter1

---
+++ flutter_app/doc/Note.md
@@ -0,0 +1,5 @@
+# FlutterExercise
+1. 引入项目：直接引入.dart文件
+2. IOS
+    1. open ios/Runner.xcworkspace 打开默认的Xcode workspace.
+    2.
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/RandomWords.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    // 第一种demo
+    /*
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -21,6 +25,23 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+
+    */
+
+    // 第二种
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
+      ),
     );
   }
 }
